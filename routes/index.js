a73c2e1787fce94c4826974f0c40111dc275625e
Christopher Shannon <200328763@student.georgianc.on.ca>
2016-10-04 20:35:10 -0400

    Added header.ejs, contact.ejs, bootstrap css

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Jamie Shannon', message: '' });
+});
+
+/* Get contact page */
+router.get('/contact', function(req, res, next){
+  res.render('contact', {title: 'Contact', message: ''});
 });
 
 module.exports = router;
