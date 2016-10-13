bef40fdeb2fe1fa9f45805df08f7805c07887feb
Christopher Shannon <200328763@student.georgianc.on.ca>
2016-10-13 15:34:42 -0400

    Added additional page links in the header and pathing for the new pages

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,28 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Jamie Shannon', message: '' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Jamie Shannon', message: ''});
 });
 
 /* Get contact page */
-router.get('/contact', function(req, res, next){
-  res.render('contact', {title: 'Contact', message: ''});
+router.get('/contact', function (req, res, next) {
+    res.render('contact', {title: 'Contact', message: ''});
+});
+
+/* Get about page*/
+router.get('/about', function (req, res, next) {
+    res.render('about', {title: 'About Me', message: ''});
+});
+
+/* Get projects page */
+router.get('/projects', function (req, res, next){
+    res.render('projects', {title: 'Projects', message: ''});
+});
+
+/* Get services page */
+router.get('/services', function (req, res, next){
+    res.render('services', {title: 'Services', message: ''});
 });
 
 module.exports = router;
